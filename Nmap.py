6a3ad35eeecdf7dac5a009246ee84249d733ffe6
Hacker M3sr <64711640+HackerM3sr@users.noreply.github.com>
2020-07-09 07:07:11 +0200

    Add files via upload

---
+++ Nmap.py
@@ -0,0 +1,24 @@
+print (" Welcome To HacKer MaSr ")
+
+import os
+
+import time
+
+def nmap().
+    os.system("clear")
+print (" wait to finsh install ")
+
+    os.system(" pkg install figlet -y >> loge.txt ")
+    print ("done install")
+    time.sleep(3)
+    os.system ("clear")
+    print ("run nmap")
+    os.system("nmap"))
+def hydra():                                                                                           os.system("clear")
+    print ("please wait")
+    os.system("pkg install hydra-y >> loge.txt")
+    print ("done install..!!!")
+    time.sleep(3)
+    os.system ("clear")
+    print ("run hydra")
+    os.system("hydra")
